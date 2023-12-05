0c163b959086d16e503ff7ed9c0148a16d976d24
samyro14 <72630697+samyro14@users.noreply.github.com>
2023-12-05 09:47:20 +0200

    Update main.cpp

--- GRILE/subiect_concurs_2022/main.cpp
+++ GRILE/subiect_concurs_2022/main.cpp
@@ -1,1 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+
+ifstream fin("f.in");
+ofstream fout("f.out");
+
+
+const int maxn = 1e6 + 2;
+/*
+Concurs UBB 2022
+
+1.C
+
+2.ABD
+Ce face algoritmul? afla ultima cifra a lui a ^(b-1) si o inmulteste cu a
+
+A. a = 107, b = 101
+    uc(107, 100) = ?
+   1. 1
+   2. 1 * 7 = 7  => 7 
+   3. 7 * 7 = 49 => 9
+   4. 9 * 7 = 63 => 3
+   5. 3 * 7 = 21 >= 1
+   6. 1 * 7 = 7  => 7
+    7 * 7 = 49 => 9
+    9 * 7 = 43 => 3
+    3 * 7 = 21 => 1
+    1 * 7 = 7 => 7
+    ......
+    => uc(a^4k) = 1
+       uc(a^(4k + 1) = 7 
+          ......
+    100 / 4 = 25
+    => A Corect
+    B. a = 1001 b = 101 => 1001?
+    uc(a^100) * a = ?
+    1 * 1001 = 1001 => 1
+    B corect
+
+
+3.AD
+
+4.
+
+5.
+
+6.
+
+7.
+
+9.
+
+10.
+
+11.
+
+12.
+
+13.
+
+14.
+
+15.
+
+16.
+
+17.
+
+18.
+
+19.
+
+20.
+
+21.
+
+22.
+
+23.
+
+24.
+*/
+int main(){
+
+
+    return 0;
+}
+
+/*
+
+
+
+
+*/
 
